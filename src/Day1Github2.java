7548d5e7bb3162b51c87c97fdadd93f090ba5f97
Metin <metinkvss@gmail.com>
2022-08-17 12:34:53 +0300

    3.C

--- src/Day1Github2.java
+++ src/Day1Github2.java
@@ -1,5 +1,7 @@
 public class Day1Github2 {
     public static void main(String[] args) {
-        System.out.println("metin");
+        System.out.println("hello git 1.deneme");
+        System.out.println("hello git 2. deneme");
+        System.out.println("hello git 3. deneme");
     }
 }
